016b1e89cd966295d90ecf91092baa43dddab202
Rudraksha Jadhav <rudraksha.jadhav@gmail.com>
2026-08-04 13:25:16 +0530

    feat: add luxury calligraphic Collections header wordmark asset

--- lib/core/widgets/navigation/top_header.dart
+++ lib/core/widgets/navigation/top_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../app/theme/app_colors.dart';
 import '../../../app/theme/app_text_styles.dart';
 
 class TopHeader extends StatelessWidget {
@@ -15,19 +16,31 @@ class TopHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isCollectionsTitle = title.toUpperCase() == 'COLLECTIONS';
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         leading ?? const SizedBox(width: 40),
         const SizedBox(width: 8),
         Expanded(
-          child: Text(
-            title,
-            textAlign: TextAlign.center,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: AppTextStyles.headline.copyWith(fontSize: 20),
-          ),
+          child: isCollectionsTitle
+              ? SizedBox(
+                  height: 38,
+                  child: Image.asset(
+                    'assets/images/collections_header.png',
+                    fit: BoxFit.contain,
+                    color: AppColors.primary,
+                    colorBlendMode: BlendMode.srcIn,
+                  ),
+                )
+              : Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyles.headline.copyWith(fontSize: 20),
+                ),
         ),
         const SizedBox(width: 8),
         trailing ?? const SizedBox(width: 40),
